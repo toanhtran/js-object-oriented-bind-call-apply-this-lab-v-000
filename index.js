6aaea763b8e106a39011b133981846019257dc7c
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-10 00:58:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function justInvoke(fn) {
   return fn();
 }
 
-function setThisWithCall(fn) {
-  return fn();
+function setThisWithCall(fn, thisValue, argument) {
+  return fn.call(thisValue, argument);
 }
